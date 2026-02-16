b302a692cf49cf4d68967692b8790c5c1053ef3d
犬塚悠介 <zucca@inudzukayuusukenoMacBook-Pro.local>
2026-02-16 17:01:31 +0900

    課題前

---
+++ db_lesson.sql
@@ -0,0 +1,18 @@
+
+--練習　レコードを作成してみよう
+/*
+INSERT INTO reports (person_id, content)
+VALUES
+('1', 'person NO1の日報です'),
+('3', 'person NO3の日報です'),
+('4', 'person NO4の日報です'),
+('5', 'person NO5の日報です'),
+('6', 'person NO6の日報です'),
+('7', 'person NO7の日報です'),
+('8', 'person NO8の日報です'),
+('9', 'person NO9の日報です'),
+('10', 'person NO10の日報です'),
+('11', 'person NO11の日報です'),
+('12', 'person NO12の日報です');
+*/
+
