06e9b027961e9f4ae300f6df271aee2011de8c9e
犬塚悠介 <zucca@inudzukayuusukenoMacBook-Pro.local>
2026-02-27 01:33:07 +0900

    課題修正Ver1

--- db_lesson.sql
+++ db_lesson.sql
@@ -17,23 +17,23 @@ VALUES
 */
 
 -- 課題No1
-/*
+
   CREATE TABLE departments (
   department_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(20) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
-*/
+
 
 -- 課題No2
-/*
+
 ALTER TABLE people
 ADD COLUMN department_id INT unsigned AFTER email;
-*/
+
 
 -- 課題No3 departments
-/*
+
 INSERT INTO departments (name)
 VALUES
 ('営業'),
@@ -41,10 +41,10 @@ VALUES
 ('経理'),
 ('人事'),
 ('情報システム');
-*/
+
 
 -- 課題No3 people
-/*
+
 INSERT INTO people (name, email, department_id, age, gender)
 VALUES 
 ('ミッキーマウス', 'mickey@Disney.com', 1, 28, 1),
@@ -57,10 +57,10 @@ VALUES
 ('ウッディ', 'woody@Disney.com', 3, 29, 1),
 ('バズライトイヤー', 'buzz@Disney.com', 4, 32, 1),
 ('ジェシー', 'jesse@Disney.com', 5, 23, 2);
-*/
+
 
 -- 課題No3 reports
-/*
+
 INSERT INTO reports (person_id, content)
 VALUES 
 (7, '今日はみんなでダンスの練習をして楽しかったよ！'),
@@ -73,10 +73,10 @@ VALUES
 (14, '仲間を守るために今日もリーダーとして頑張った。'),
 (15, '宇宙の平和を守る任務は今日も順調だった。'),
 (16, 'ロデオの練習をして汗をかいたけど楽しかった！');
-*/
+
 
 -- 課題No4
-/*
+
 UPDATE people
 SET department_id = 3
 WHERE department_id IS NULL
@@ -96,16 +96,18 @@ UPDATE people
 SET department_id = 1
 WHERE department_id IS NULL
 AND age IS NULL;
-*/
+
 
 -- 課題No5
-/*
+
 SELECT name, age
 FROM people
+WHERE gender = 1
 ORDER BY age DESC;
-*/
+
 
 -- 課題No6
+
 /*
 SELECT
   `name`, `email`, `age`
@@ -115,47 +117,48 @@ WHERE
   `department_id` = 1
 ORDER BY
   `created_at`;
+*/
 
 日本語で説明
   peopleというテーブルから
   name email ageという名のカラムを取得している
 　また、department_idが1のレコードだけに絞っている
-　そして当てはまるレコードをcreated_at（作成時間）順で並べています
-*/
+　そして当てはまるレコードをcreated_at（作成時間）昇順で並べています
+
 
 -- 課題No7
-/*
+
 SELECT name
 FROM people
 WHERE (age BETWEEN 20 AND 29 AND gender = 2)
 OR (age BETWEEN 40 AND 49 AND gender = 1);
-*/
+
 
 -- 課題No8
-/*
+
 SELECT name, age
 FROM people
 WHERE department_id = 1
 ORDER BY age ASC;
-*/
+
 
 -- 課題No9
-/*
+
 SELECT AVG(age) AS average_age
 FROM people
 WHERE department_id = 2
 AND gender = 2;
-*/
+
 
 -- 課題No10
-/*
+
 SELECT p.name, d.name, r.content
 FROM people p
 JOIN departments d
 ON p.department_id = d.department_id
 JOIN reports r
 ON p.person_id = r.person_id;
-*/
+
 
 -- 課題No11
 
